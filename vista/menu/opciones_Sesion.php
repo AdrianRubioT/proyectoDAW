f5d269b57ac149eee9a7ceba141bc180bff1f3d0
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-02-08 15:40:31 +0000

    paso a malluscula Opcione sd esesion

--- vista/menu/opciones_Sesion.php
+++ vista/menu/opciones_Sesion.php
@@ -2,7 +2,7 @@
 <p><?php echo "Hola ".$usuario->getUser(); ?></p>
 
 <ul class="desplegable">
-    <li> <p>opciones de sesion</p>
+    <li> <p>Opciones de sesion</p>
         <ul>
             <li><a id="enlacePerfil" href="perfil.php">perfil</a></li>
             <li><a id="cerrarSesion" href="controlador/user/sesion/logout.php">cerrar Sesion</a></li>
