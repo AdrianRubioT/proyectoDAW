bc9714556f5556703502a6a25b4d2800beba017d
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-02-11 15:01:27 +0000

    modificado la obtencion de la ip, no requiere mas de la var ip a mano, se usa window.location.host y quitado los console.log debugs

--- scripts/puntuaciones.js
+++ scripts/puntuaciones.js
@@ -1,12 +1,16 @@
 
 var peticion_http = new XMLHttpRequest();
 
-var ip = "18.224.32.38";
-var url = "http://"+ip+":8080/proyectoDAW/funciones/guardarPuntuacion.php";
 
-// function cargar(){
-//     enviarPuntuacion(4);
-// }
+//console.log(window.location.host);
+var ip = window.location.host;
+//var ip = "18.224.32.38";
+var url = "http://"+ip+"/proyectoDAW/funciones/guardarPuntuacion.php";
+
+function cargar(){
+    enviarPuntuacion(4);
+    //console.log("enviado");
+}
 
 
 
@@ -15,16 +19,18 @@ function enviarPuntuacion(puntuacion){
     var ID_juego = getUrlVars()["ID_juego"];
     
     peticion_http.onreadystatechange = mostrar;
-    
-    peticion_http.open('GET', url + "?" + "IDjuego="+ID_juego +"&"+ "puntuacion="+puntuacion, true);
-    
+    //console.log(url + "?" + "IDjuego="+ ID_juego +"&"+ "puntuacion="+puntuacion);
+    peticion_http.open('GET', url + "?" + "IDjuego="+ ID_juego +"&"+ "puntuacion="+puntuacion, true);
+    peticion_http.send(null);
     
 }
 
 function mostrar() {
+    //console.log(peticion_http.readyState);
     if(peticion_http.readyState == 4 && peticion_http.status == 200) {
         //lugar para posible funcionalidad 
-        //console.log(peticion_http.responseText);
+        console.log(peticion_http.responseText);
+        // console.log("respuesta");
     }
 }
 
@@ -40,4 +46,4 @@ function getUrlVars() {
 }
 
 
-//document.addEventListener("DOMContentLoaded", cargar, false);
+document.addEventListener("DOMContentLoaded", cargar, false);
