7c5533b4086e1de41cb9276222a5c8d486913904
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-02-11 16:56:15 +0000

    mal nombrado  renombrado

--- cabecera/impors.php
+++
@@ -1,7 +0,0 @@
-<?php require_once("controlador/user/usuario.php") ?>
-
-<?php require_once("cabecera/cookies/cookies.php");  ?>
-
-<?php require_once("modelo/DB/consultasMSQL.php"); ?>
-
-<?php require_once("controlador/user/sesion/login.php"); ?>
