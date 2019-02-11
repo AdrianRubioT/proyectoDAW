1b2d85f7278237503a254cf2a1a13919e244c5b1
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-02-11 14:59:50 +0000

    priemra primera prueba: recojer todas las variables y guardalo en la base de datos

--- funciones/guardarPuntuacion.php
+++ funciones/guardarPuntuacion.php
@@ -1,23 +1,21 @@
 <?php
 
     require("../controlador/user/usuario.php");
+    require_once("../modelo/DB/consultasMSQL.php");
+    
+    // echo "php: recivido";
+    // echo $_GET["IDjuego"];
+    // echo $_GET["puntuacion"];
+    // var_dump($_GET);
 
-    echo $_GET["IDjuego"];
-
-    echo $_GET["puntuacion"];
-    
-    //var_dump($_GET);
-    
-    
     session_start();
-
     if(isset($_SESSION["usuario"])){
-        //echo "hay variable de sesion usuario";
+        //requiere la clase de usuario para el unserialize
         $usuario = unserialize($_SESSION["usuario"]);
-        echo $usuario->getUser();
+        //echo $usuario->getUser();
     }
 
-    
-
+    guardarPuntuacion($_GET["IDjuego"], $_GET["puntuacion"] ,$usuario->getCorreo() );
+    // echo "terminado";
 
 ?>
