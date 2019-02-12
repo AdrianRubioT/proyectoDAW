8ae929dc7b82b0c9088b7d09873d95d1f4f2dd15
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-02-12 17:54:59 +0000

    intento de ser exportado fallido

--- cabecera/scripts/puntuaciones.js
+++ cabecera/scripts/puntuaciones.js
@@ -1,4 +1,3 @@
-
 var peticion_http = new XMLHttpRequest();
 
 
@@ -12,22 +11,28 @@ var ip = window.location.host;
 var url = "http://"+ip+"/proyectoDAW/guardarPuntuacion.php";
 
 
-function enviarPuntuacion(puntuacion){
+export function enviarPuntuacion(puntuacion){
     
     var ID_juego = getUrlVars()["ID_juego"];
     
     peticion_http.onreadystatechange = mostrar;
     //console.log(url + "?" + "IDjuego="+ ID_juego +"&"+ "puntuacion="+puntuacion);
+
+    
+
     peticion_http.open('GET', url + "?" + "IDjuego="+ ID_juego +"&"+ "puntuacion="+puntuacion, true);
     peticion_http.send(null);
     
 }
+
+
 
 function mostrar() {
     //console.log(peticion_http.readyState);
     if(peticion_http.readyState == 4 && peticion_http.status == 200) {
         //lugar para posible funcionalidad 
         
+            alert(peticion_http.responseText);
         if(peticion_http.responseText != ""){
             alert(peticion_http.responseText);
         }
