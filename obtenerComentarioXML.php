b5af1ca9c311a0dd30eba20ed4019b70174d5f7e
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-02-14 18:15:42 +0000

    phph que entrega un xml con los comentarios

--- obtenerComentarioXML.php
+++ obtenerComentarioXML.php
@@ -1,49 +1,44 @@
 <?php require_once("controlador/user/usuario.php") ?>
-
 <?php require_once("cabecera/cookies/cookies.php");  ?>
-
 <?php require_once("modelo/DB/consultasMSQL.php"); ?>
-
 <?php
 
-// fiehcaro para crear el xml con los comentarios
-
+// archivo para crear el xml con los comentarios
 
 header ("Content-Type: application/xml");
 
         $pagina = 0;
         $numElementos = 5;
 
-    $idMensages = obtenerMensagesPadreJuegos($_GET["page"]);
-    
+    $idMensages = obtenerMensagesPadreJuegos($_GET["ID_juego"], $pagina, $numElementos);
+    // var_dump($idMensages);
     echo "<comentarios>";
     
     foreach($idMensages as $key => $value){
-        imprimirComentario($value, $pagina, $numElementos);
+        imprimirComentario($value);
     }
     
     echo "</comentarios>";
     
     function imprimirComentario($ID_men){
+        // echo $ID_men;
         echo "<comentario>";
-        // $pagina = 0;
-        // $numElementos = 5;
-        
+
         $respuestas = obtenerIDMenRespuesta($ID_men);
         $comentario = obtenerMensage($ID_men);
+        // var_dump($comentario);
             echo "<nombre>";
-            echo $comentario["nombre"];
+            echo $comentario["Nombre"];
             echo "</nombre>";
             echo "<texto>";
-            echo $comentario["texto"];
+            echo $comentario["Texto"];
             echo "</texto>";
         
-        if(!is_null($respuestas)){
+        if($respuestas != false){
             
             foreach($respuestas as $key => $value){
                 
                 imprimirComentario($value);
-                
             }    
         }
         
