5551eec3ac029943a82743eb14dd762aa1c1e3aa
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-02-19 21:46:51 +0000

    aniadido if para evitar problemas a la hora de guardar el comentario en la base de datos

--- guardarComentario.php
+++ guardarComentario.php
@@ -9,6 +9,12 @@
 <?php
     
     // var_dump($_GET);
-    guardarComentario($_GET["ID_juego"], $_GET["textoComentario"], "null", $usuario->getCorreo());
+    
+    // si ID padre no se da se pone a string null para la base de datos
+    if(!isset($_GET["ID_padre"])){
+        $_GET["ID_padre"] = "null";
+    }
+    
+    guardarComentario($_GET["ID_juego"], $_GET["textoComentario"], $_GET["ID_padre"], $usuario->getCorreo());
     header('location:/proyectoDAW/jugar.php?ID_juego='.$_GET["ID_juego"]);
 ?>
