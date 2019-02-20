a3ff91a0ecaddeeb76f5a47d65299ebe7470dbca
AdrianRubioT <adrianalumnosmx@gmail.com>
2019-02-20 20:01:50 +0000

    comentados los enlaces que no tiene funcionalidad

--- vista/menu/menu.php
+++ vista/menu/menu.php
@@ -3,19 +3,19 @@
 <nav id="menu">
         
         <div id="linea1">
-            <div id="logo"><img src="img/logo.png"></img></div>
+            <div id="logo"><a class="enlaceBiblioteca" href="index.php"><img src="img/logo.png"></img></a></div>
             <div id="user"><?php require_once("controlador/menu/menuL1.php"); ?></div>
         </div>
         
         
-        <div id="linea2">
+        <!--<div id="linea2">-->
             <!--<div id="categorias"></div>-->
-            <div id="accesoRapido" class=" botonera">
-                <a class="enlaceBiblioteca boton" href="index.php">Inicio</a>
-                <a class="enlaceBiblioteca boton" href="">Biblioteca</a>
-                <a class="enlaceBiblioteca boton" href="">Categorias</a>
-            </div>
-        </div>
+        <!--    <div id="accesoRapido" class=" botonera">-->
+                <!--<a class="enlaceBiblioteca boton" href="index.php">Inicio</a>-->
+                <!--<a class="enlaceBiblioteca boton" href="">Biblioteca</a>-->
+                <!--<a class="enlaceBiblioteca boton" href="">Categorias</a>-->
+        <!--    </div>-->
+        <!--</div>-->
             
 </nav>
 
